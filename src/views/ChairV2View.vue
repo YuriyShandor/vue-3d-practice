b57cd477f12f8313b482f5cd9ef07d15e0d9e730
YuriyShandor <yuriyshandor@gmail.com>
2023-12-29 16:12:47 +0200

    -wip --- add chair model with variants

--- src/views/ChairV2View.vue
+++ src/views/ChairV2View.vue
@@ -9,8 +9,8 @@
     </div>
     <div class="w-auto absolute top-[60px] left-[10px] flex gap-[5px] z-20">
       <div
-        class="w-[55px] h-[55px] px-[10px] py-[10px] bg-slate-200 hover:bg-slate-300 rounded-md flex items-center justify-center cursor-pointer transition-colors duration-200"
-        :class="{ 'bg-slate-500': state.activeOption === item.label }"
+        class="w-[55px] h-[55px] px-[10px] py-[10px] bg-slate-500 hover:bg-slate-600 rounded-md flex items-center justify-center cursor-pointer transition-colors duration-200"
+        :class="{ '!bg-slate-700': state.activeOption === item.label }"
         data-option="item.label"
         v-for="item in details"
         :key="item.label"
@@ -62,7 +62,6 @@ const state = reactive({
 const BACKGROUND_COLOR = 0xf1f1f1;
 
 let camera: any, scene: any, renderer: any;
-let parser: any, variantsExtension: any;
 
 let sceneContainer: HTMLElement | null = null;
 
@@ -76,12 +75,7 @@ const INITIAL_MAP = [
   { childID: 'supports', mtl: INITIAL_MTL }
 ];
 
-// let LOADER: any = null;
-let DRAG_NOTICE: any = null;
-let TRAY: any = null;
 let theModel: any;
-let loaded = false;
-let cameraFar = 5;
 
 const details = [
   {
@@ -298,16 +292,18 @@ const resizeScene = () => {
   if (sceneContainer !== null) {
     sceneContainer.style.height = `${sceneContainer.clientWidth}px`;
     renderer.setSize(sceneContainer.clientWidth, sceneContainer.clientHeight);
+
+    camera.updateProjectionMatrix();
   }
 };
 
 const initScene = () => {
   scene = new Scene();
-  scene.background = new Color(BACKGROUND_COLOR);
+  scene.background = new Color(0x141414);
   scene.fog = new Fog(BACKGROUND_COLOR, 20, 100);
 
   camera = new PerspectiveCamera(50, 1, 0.1, 1000);
-  camera.position.z = cameraFar;
+  camera.position.z = 5;
   camera.position.x = 0;
 
   const canvas: HTMLCanvasElement | null = document.querySelector('#canvas');
@@ -342,30 +338,6 @@ const initSceneLights = () => {
   scene.add(dirLight);
 };
 
-const initSceneFloor = () => {
-  const floorGeometry = new PlaneGeometry(5000, 5000, 1, 1);
-  const floorMaterial = new MeshPhongMaterial({
-    color: 0xeeeeee,
-    shininess: 0
-  });
-
-  const floor = new Mesh(floorGeometry, floorMaterial);
-  floor.rotation.x = -0.5 * Math.PI;
-  floor.receiveShadow = true;
-  floor.position.y = -1;
-  scene.add(floor);
-};
-
-const initControls = () => {
-  const controls = new OrbitControls(camera, renderer.domElement);
-  // controls.maxPolarAngle = Math.PI / 2;
-  // controls.minPolarAngle = Math.PI / 3;
-  controls.enableDamping = true;
-
-  controls.update();
-  renderer.render(scene, camera);
-};
-
 const initColor = (parent: any, type: any, mtl: any) => {
   parent.traverse((o: any) => {
     if (o.isMesh) {
@@ -389,21 +361,16 @@ const loadModel = () => {
       }
     });
 
-    // Set the models initial scale
     theModel.scale.set(2, 2, 2);
     theModel.rotation.y = Math.PI;
 
-    // Add the model to the scene
     theModel.position.y = -1;
 
-    // Set initial textures
     for (let object of INITIAL_MAP) {
       initColor(theModel, object.childID, object.mtl);
     }
 
     scene.add(theModel);
-
-    // LOADER.remove();
   });
 };
 
@@ -451,473 +418,6 @@ onMounted(() => {
   initScene();
   resizeScene();
   renderScene();
-
-  // initScene();
-  // resizeScene();
-  // renderScene();
-
-  // window.addEventListener('resize', resizeScene);
-
-  // LOADER = document.getElementById('js-loader');
-  // DRAG_NOTICE = document.getElementById('js-drag-notice');
-  // TRAY = document.getElementById('js-tray-slide');
-
-  // // function animate() {
-  // //   controls.update();
-  // //   renderer.render(scene, camera);
-  // //   requestAnimationFrame(animate);
-
-  // //   if (resizeRendererToDisplaySize(renderer)) {
-  // //     const canvas = renderer.domElement;
-  // //     camera.aspect = canvas.clientWidth / canvas.clientHeight;
-  // //     camera.updateProjectionMatrix();
-  // //   }
-
-  // //   if (theModel != null && loaded == false) {
-  // //     initialRotation();
-  // //     DRAG_NOTICE.classList.add('start');
-  // //   }
-  // // }
-
-  // // animate();
-
-  // // Function - Build Colors
-
-  // function buildColors(colors) {
-  //   for (let [i, color] of colors.entries()) {
-  //     let swatch = document.createElement('div');
-  //     swatch.classList.add('tray__swatch');
-
-  //     if (color.texture) {
-  //       swatch.style.backgroundImage = 'url(' + color.texture + ')';
-  //     } else {
-  //       swatch.style.background = '#' + color.color;
-  //     }
-
-  //     swatch.setAttribute('data-key', i);
-  //     TRAY.append(swatch);
-  //   }
-  // }
-
-  // buildColors(colors);
-
-  // // Select Option
-  // const options = document.querySelectorAll('.option');
-
-  // for (const option of options) {
-  //   option.addEventListener('click', selectOption);
-  // }
-
-  // function selectOption(e) {
-  //   let option = e.target;
-  //   activeOption = e.target.dataset.option;
-  //   for (const otherOption of options) {
-  //     otherOption.classList.remove('--is-active');
-  //   }
-  //   option.classList.add('--is-active');
-  // }
-
-  // // Swatches
-  // const swatches = document.querySelectorAll('.tray__swatch');
-
-  // for (const swatch of swatches) {
-  //   swatch.addEventListener('click', selectSwatch);
-  // }
-
-  // function selectSwatch(e) {
-  //   let color = colors[parseInt(e.target.dataset.key)];
-  //   let new_mtl;
-
-  //   if (color.texture) {
-  //     let txt = new TextureLoader().load(color.texture);
-
-  //     txt.repeat.set(color.size[0], color.size[1], color.size[2]);
-  //     txt.wrapS = RepeatWrapping;
-  //     txt.wrapT = RepeatWrapping;
-
-  //     new_mtl = new MeshPhongMaterial({
-  //       map: txt,
-  //       shininess: color.shininess ? color.shininess : 10
-  //     });
-  //   } else {
-  //     new_mtl = new MeshPhongMaterial({
-  //       color: parseInt('0x' + color.color),
-  //       shininess: color.shininess ? color.shininess : 10
-  //     });
-  //   }
-
-  //   setMaterial(theModel, activeOption, new_mtl);
-  // }
-
-  // function setMaterial(parent, type, mtl) {
-  //   parent.traverse((o) => {
-  //     if (o.isMesh && o.nameID != null) {
-  //       if (o.nameID == type) {
-  //         o.material = mtl;
-  //       }
-  //     }
-  //   });
-  // }
-
-  // // // Function - Opening rotate
-  // // let initRotate = 0;
-
-  // // function initialRotation() {
-  // //   initRotate++;
-  // //   if (initRotate <= 120) {
-  // //     theModel.rotation.y += Math.PI / 60;
-  // //   } else {
-  // //     loaded = true;
-  // //   }
-  // // }
-
-  // // var slider = document.getElementById('js-tray'),
-  // //   sliderItems = document.getElementById('js-tray-slide'),
-  // //   difference;
-
-  // // function slide(wrapper, items) {
-  // //   var posX1 = 0,
-  // //     posX2 = 0,
-  // //     posInitial,
-  // //     threshold = 20,
-  // //     posFinal,
-  // //     slides = items.getElementsByClassName('tray__swatch');
-
-  // //   // Mouse events
-  // //   items.onmousedown = dragStart;
-
-  // //   // Touch events
-  // //   items.addEventListener('touchstart', dragStart);
-  // //   items.addEventListener('touchend', dragEnd);
-  // //   items.addEventListener('touchmove', dragAction);
-
-  // //   function dragStart(e) {
-  // //     e = e || window.event;
-  // //     posInitial = items.offsetLeft;
-  // //     difference = sliderItems.offsetWidth - slider.offsetWidth;
-  // //     difference = difference * -1;
-
-  // //     if (e.type == 'touchstart') {
-  // //       posX1 = e.touches[0].clientX;
-  // //     } else {
-  // //       posX1 = e.clientX;
-  // //       document.onmouseup = dragEnd;
-  // //       document.onmousemove = dragAction;
-  // //     }
-  // //   }
-
-  // //   function dragAction(e) {
-  // //     e = e || window.event;
-
-  // //     if (e.type == 'touchmove') {
-  // //       posX2 = posX1 - e.touches[0].clientX;
-  // //       posX1 = e.touches[0].clientX;
-  // //     } else {
-  // //       posX2 = posX1 - e.clientX;
-  // //       posX1 = e.clientX;
-  // //     }
-
-  // //     if (items.offsetLeft - posX2 <= 0 && items.offsetLeft - posX2 >= difference) {
-  // //       items.style.left = items.offsetLeft - posX2 + 'px';
-  // //     }
-  // //   }
-
-  // //   function dragEnd(e) {
-  // //     posFinal = items.offsetLeft;
-  // //     if (posFinal - posInitial < -threshold) {
-  // //     } else if (posFinal - posInitial > threshold) {
-  // //     } else {
-  // //       items.style.left = posInitial + 'px';
-  // //     }
-
-  // //     document.onmouseup = null;
-  // //     document.onmousemove = null;
-  // //   }
-  // // }
-
-  // // slide(slider, sliderItems);
-
-  // LOADER = document.getElementById('js-loader');
-  // DRAG_NOTICE = document.getElementById('js-drag-notice');
-  // TRAY = document.getElementById('js-tray-slide');
-
-  // const MODEL_PATH = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/chair.glb';
-
-  // initScene();
-  // Init the scene
-  // scene = new Scene();
-  // // Set background
-  // scene.background = new Color(BACKGROUND_COLOR);
-  // scene.fog = new Fog(BACKGROUND_COLOR, 20, 100);
-
-  // const canvas = document.querySelector('#canvas');
-  // renderer = new WebGLRenderer({ canvas, antialias: true });
-  // renderer.shadowMap.enabled = true;
-  // renderer.setPixelRatio(window.devicePixelRatio);
-
-  // renderScene();
-
-  // Add a camera
-  // var camera = new PerspectiveCamera(50, 1, 0.1, 1000);
-  // camera.position.z = cameraFar;
-  // camera.position.x = 0;
-
-  // Initial material
-  // const INITIAL_MTL = new MeshPhongMaterial({ color: 0xf1f1f1, shininess: 10 });
-
-  // const INITIAL_MAP = [
-  //   { childID: 'back', mtl: INITIAL_MTL },
-  //   { childID: 'base', mtl: INITIAL_MTL },
-  //   { childID: 'cushions', mtl: INITIAL_MTL },
-  //   { childID: 'legs', mtl: INITIAL_MTL },
-  //   { childID: 'supports', mtl: INITIAL_MTL }
-  // ];
-
-  // Init the object loader
-  // var loader = new GLTFLoader();
-
-  // loader.load(
-  //   MODEL_PATH,
-  //   function (gltf) {
-  //     theModel = gltf.scene;
-
-  //     theModel.traverse((o) => {
-  //       if (o.isMesh) {
-  //         o.castShadow = true;
-  //         o.receiveShadow = true;
-  //       }
-  //     });
-
-  //     // Set the models initial scale
-  //     theModel.scale.set(2, 2, 2);
-  //     theModel.rotation.y = Math.PI;
-
-  //     // Add the model to the scene
-  //     theModel.position.y = -1;
-
-  //     // Set initial textures
-  //     for (let object of INITIAL_MAP) {
-  //       initColor(theModel, object.childID, object.mtl);
-  //     }
-
-  //     scene.add(theModel);
-
-  //     // Remove the loader
-  //     LOADER.remove();
-  //   },
-  //   undefined,
-  //   function (error) {
-  //     console.error(error);
-  //   }
-  // );
-
-  // Function - Add the textures to the models
-  // function initColor(parent, type, mtl) {
-  //   parent.traverse((o) => {
-  //     if (o.isMesh) {
-  //       if (o.name.includes(type)) {
-  //         o.material = mtl;
-  //         o.nameID = type; // Set a new property to identify this object
-  //       }
-  //     }
-  //   });
-  // }
-
-  // // Add lights
-  // var hemiLight = new HemisphereLight(0xffffff, 0xffffff, 0.61);
-  // hemiLight.position.set(0, 50, 0);
-  // // Add hemisphere light to scene
-  // scene.add(hemiLight);
-
-  // var dirLight = new DirectionalLight(0xffffff, 0.54);
-  // dirLight.position.set(-8, 12, 8);
-  // dirLight.castShadow = true;
-  // dirLight.shadow.mapSize = new Vector2(1024, 1024);
-  // // Add directional Light to scene
-  // scene.add(dirLight);
-
-  // initSceneFloor();
-
-  // // Floor
-  // var floorGeometry = new PlaneGeometry(5000, 5000, 1, 1);
-  // var floorMaterial = new MeshPhongMaterial({
-  //   color: 0xeeeeee, // This color is manually dialed in to match the background color
-  //   shininess: 0
-  // });
-
-  // var floor = new Mesh(floorGeometry, floorMaterial);
-  // floor.rotation.x = -0.5 * Math.PI;
-  // floor.receiveShadow = true;
-  // floor.position.y = -1;
-  // scene.add(floor);
-
-  // initControls();
-
-  // // Add controls
-  // var controls = new OrbitControls(camera, renderer.domElement);
-  // // controls.maxPolarAngle = Math.PI / 2;
-  // // controls.minPolarAngle = Math.PI / 3;
-  // controls.enableDamping = true;
-
-  // function animate() {
-  //   // controls.update();
-  //   // renderer.render(scene, camera);
-  //   requestAnimationFrame(animate);
-
-  //   if (resizeRendererToDisplaySize(renderer)) {
-  //     const canvas = renderer.domElement;
-  //     camera.aspect = canvas.clientWidth / canvas.clientHeight;
-  //     camera.updateProjectionMatrix();
-  //   }
-
-  //   if (theModel != null && loaded == false) {
-  //     // initialRotation();
-  //     DRAG_NOTICE.classList.add('start');
-  //   }
-  // }
-
-  // animate();
-
-  // Function - New resizing method
-  // function resizeRendererToDisplaySize(renderer) {
-  //   const canvas = renderer.domElement;
-  //   var width = window.innerWidth;
-  //   var height = window.innerHeight;
-  //   var canvasPixelWidth = canvas.width / window.devicePixelRatio;
-  //   var canvasPixelHeight = canvas.height / window.devicePixelRatio;
-
-  //   const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
-  //   if (needResize) {
-  //     renderer.setSize(width, height, false);
-  //   }
-  //   return needResize;
-  // }
-
-  // Disable scrolling .. (??)
-  // window.onscroll = function () {
-  //   window.scrollTo(0, 0);
-  // };
-
-  // Function - Build Colors
-
-  // function buildColors(colors) {
-  //   for (let [i, color] of colors.entries()) {
-  //     let swatch = document.createElement('div');
-  //     swatch.classList.add('tray__swatch');
-
-  //     if (color.texture) {
-  //       swatch.style.backgroundImage = 'url(' + color.texture + ')';
-  //     } else {
-  //       swatch.style.background = '#' + color.color;
-  //     }
-
-  //     swatch.setAttribute('data-key', i);
-  //     TRAY.append(swatch);
-  //   }
-  // }
-
-  // buildColors(colors);
-
-  // Select Option
-  // const options = document.querySelectorAll('.option');
-
-  // for (const option of options) {
-  //   option.addEventListener('click', selectOption);
-  // }
-
-  // function selectOption(e) {
-  //   let option = e.target;
-  //   activeOption = e.target.dataset.option;
-  //   for (const otherOption of options) {
-  //     otherOption.classList.remove('--is-active');
-  //   }
-  //   option.classList.add('--is-active');
-  // }
-
-  // // Swatches
-  // const swatches = document.querySelectorAll('.tray__swatch');
-
-  // for (const swatch of swatches) {
-  //   swatch.addEventListener('click', selectSwatch);
-  // }
-
-  // Function - Opening rotate
-  // let initRotate = 0;
-
-  // function initialRotation() {
-  //   initRotate++;
-  //   if (initRotate <= 120) {
-  //     theModel.rotation.y += Math.PI / 60;
-  //   } else {
-  //     loaded = true;
-  //   }
-  // }
-
-  // var slider = document.getElementById('js-tray'),
-  //   sliderItems = document.getElementById('js-tray-slide'),
-  //   difference;
-
-  // function slide(wrapper, items) {
-  //   var posX1 = 0,
-  //     posX2 = 0,
-  //     posInitial,
-  //     threshold = 20,
-  //     posFinal,
-  //     slides = items.getElementsByClassName('tray__swatch');
-
-  //   // Mouse events
-  //   items.onmousedown = dragStart;
-
-  //   // Touch events
-  //   items.addEventListener('touchstart', dragStart);
-  //   items.addEventListener('touchend', dragEnd);
-  //   items.addEventListener('touchmove', dragAction);
-
-  //   // function dragStart(e) {
-  //   //   e = e || window.event;
-  //   //   posInitial = items.offsetLeft;
-  //   //   difference = sliderItems.offsetWidth - slider.offsetWidth;
-  //   //   difference = difference * -1;
-
-  //   //   if (e.type == 'touchstart') {
-  //   //     posX1 = e.touches[0].clientX;
-  //   //   } else {
-  //   //     posX1 = e.clientX;
-  //   //     document.onmouseup = dragEnd;
-  //   //     document.onmousemove = dragAction;
-  //   //   }
-  //   // }
-
-  //   // function dragAction(e) {
-  //   //   e = e || window.event;
-
-  //   //   if (e.type == 'touchmove') {
-  //   //     posX2 = posX1 - e.touches[0].clientX;
-  //   //     posX1 = e.touches[0].clientX;
-  //   //   } else {
-  //   //     posX2 = posX1 - e.clientX;
-  //   //     posX1 = e.clientX;
-  //   //   }
-
-  //   //   if (items.offsetLeft - posX2 <= 0 && items.offsetLeft - posX2 >= difference) {
-  //   //     items.style.left = items.offsetLeft - posX2 + 'px';
-  //   //   }
-  //   // }
-
-  //   // function dragEnd(e) {
-  //   //   posFinal = items.offsetLeft;
-  //   //   if (posFinal - posInitial < -threshold) {
-  //   //   } else if (posFinal - posInitial > threshold) {
-  //   //   } else {
-  //   //     items.style.left = posInitial + 'px';
-  //   //   }
-
-  //   //   document.onmouseup = null;
-  //   //   document.onmousemove = null;
-  //   // }
-  // }
-
-  // slide(slider, sliderItems);
 });
 
 onUnmounted(() => {
