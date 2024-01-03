079be33d24c55e6462f0285e0a92dcaeae41d359
YuriyShandor <yuriyshandor@gmail.com>
2024-01-03 17:16:47 +0200

    -wip --- add cube model

--- src/views/CubeModelView.vue
+++ src/views/CubeModelView.vue
@@ -10,7 +10,7 @@
         </div>
         <div class="w-full flex flex-col gap-3 absolute top-[50px] left-0 z-20">
           <div class="flex gap-2">
-            <div class="text-[18px] font-bold">First Cube X:</div>
+            <div class="text-[18px] font-bold">Cube X:</div>
             <label for="input_radius">
               <input
                 type="number"
@@ -24,7 +24,7 @@
             </label>
           </div>
           <div class="flex gap-2">
-            <div class="text-[18px] font-bold">First Cube Y:</div>
+            <div class="text-[18px] font-bold">Cube Y:</div>
             <label for="input_radius">
               <input
                 type="number"
@@ -38,7 +38,7 @@
             </label>
           </div>
           <div class="flex gap-2">
-            <div class="text-[18px] font-bold">First Cube Z:</div>
+            <div class="text-[18px] font-bold">Cube Z:</div>
             <label for="input_radius">
               <input
                 type="number"
@@ -52,7 +52,7 @@
             </label>
           </div>
           <div class="flex gap-2">
-            <div class="text-[18px] font-bold">Color:</div>
+            <div class="text-[18px] font-bold">Cube Color:</div>
             <label for="input_radius">
               <input
                 type="text"
@@ -60,6 +60,60 @@
                 class="text-black"
                 v-model="state.cubeColor"
                 @change="updateFirstCubeColor()"
+              />
+            </label>
+          </div>
+          <div class="flex gap-2">
+            <div class="text-[18px] font-bold">Cylinder X:</div>
+            <label for="input_radius">
+              <input
+                type="number"
+                id="input_radius"
+                class="text-black"
+                min="0.5"
+                max="10"
+                step="0.1"
+                v-model="state.cylinderX"
+              />
+            </label>
+          </div>
+          <div class="flex gap-2">
+            <div class="text-[18px] font-bold">Cylinder Y:</div>
+            <label for="input_radius">
+              <input
+                type="number"
+                id="input_radius"
+                class="text-black"
+                min="0.5"
+                max="10"
+                step="0.1"
+                v-model="state.cylinderY"
+              />
+            </label>
+          </div>
+          <div class="flex gap-2">
+            <div class="text-[18px] font-bold">Cylinder Z:</div>
+            <label for="input_radius">
+              <input
+                type="number"
+                id="input_radius"
+                class="text-black"
+                min="0.5"
+                max="10"
+                step="0.1"
+                v-model="state.cylinderZ"
+              />
+            </label>
+          </div>
+          <div class="flex gap-2">
+            <div class="text-[18px] font-bold">Cylinder Color:</div>
+            <label for="input_radius">
+              <input
+                type="text"
+                id="input_radius"
+                class="text-black"
+                v-model="state.cylinderColor"
+                @change="updateCylinderCubeColor()"
               />
             </label>
           </div>
@@ -71,7 +125,14 @@
 
 <script setup lang="ts">
 import { onMounted, onUnmounted, reactive, watch } from 'vue';
-import { Scene, PerspectiveCamera, WebGLRenderer, HemisphereLight, MeshPhongMaterial } from 'three';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  HemisphereLight,
+  MeshPhongMaterial,
+  AmbientLight
+} from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
@@ -79,13 +140,17 @@ const state = reactive({
   cubeX: 1,
   cubeY: 1,
   cubeZ: 1,
-  cubeColor: ''
+  cubeColor: '',
+  cylinderX: 1,
+  cylinderY: 1,
+  cylinderZ: 1,
+  cylinderColor: ''
 });
 
 let camera: any, scene: any, renderer: any;
 let controls: any;
 
-let cube: any;
+let cube: any, cylinder: any, ball: any;
 
 let sceneContainer: HTMLElement | null = null;
 
@@ -109,8 +174,8 @@ const resizeScene = () => {
 
 const initScene = () => {
   scene = new Scene();
-  scene.background = null;
-  scene.add(new HemisphereLight(0xffffff, 0xffffff, 1.0));
+  scene.background = '#000000';
+  scene.add(new AmbientLight(0xffffff, 0.6));
   camera = new PerspectiveCamera(33, 1, 0.1, 1000);
   camera.position.set(13, 3, 3);
   // camera.position.z = 15;
@@ -143,8 +208,19 @@ const loadModel = () => {
 
     controls.target.copy(gltf.scene.position);
 
+    console.log(gltf.scene);
+
+    gltf.scene.children.forEach((item) => {
+      if (item.name === 'Cube') {
+        cube = item;
+      } else if (item.name === 'Mball') {
+        ball = item;
+      } else if (item.name === 'Cylinder') {
+        cylinder = item;
+      }
+    });
+
     cube = gltf.scene.children[0];
-    console.log(cube);
     cube.scale.x = state.cubeX;
     cube.scale.y = state.cubeY;
     cube.scale.z = state.cubeZ;
@@ -157,9 +233,14 @@ const updateFirstCubeColor = () => {
     shininess: 10
   });
   render();
+};
 
-  console.log(state.cubeColor);
-  console.log(cube.material);
+const updateCylinderCubeColor = () => {
+  cylinder.material = new MeshPhongMaterial({
+    color: state.cylinderColor,
+    shininess: 10
+  });
+  render();
 };
 
 onMounted(() => {
@@ -196,6 +277,30 @@ watch(
   () => state.cubeZ,
   () => {
     cube.scale.z = state.cubeZ;
+    render();
+  }
+);
+
+watch(
+  () => state.cylinderX,
+  () => {
+    cylinder.scale.x = state.cylinderX;
+    render();
+  }
+);
+
+watch(
+  () => state.cylinderY,
+  () => {
+    cylinder.scale.y = state.cylinderY;
+    render();
+  }
+);
+
+watch(
+  () => state.cylinderZ,
+  () => {
+    cylinder.scale.z = state.cylinderZ;
     render();
   }
 );
